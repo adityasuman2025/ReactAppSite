74a9ce0c1cb9b709ca9c0551caa53fb2eaf36914
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-04-21 02:20:11 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import './App.css';
 
@@ -15,10 +15,13 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">       
           <Navbar />
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/:post_id' component={Post} />
+
+          <Switch> {/* switch is required to make work only one route at a moment*/}
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
